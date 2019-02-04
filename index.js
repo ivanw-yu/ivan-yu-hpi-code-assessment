85031bc3e2ed9a3ed99468353d3f45a4392aec0a
ivanw-yu <ivnyu@mail.sfsu.edu>
2019-02-04 07:26:57 -0800

    Fixed minor bug by importing the path module to index.js. Deploying to heroku.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
+const path = require('path');
 
 const db = require('./config/db');
 mongoose.connect(db.url, { useNewUrlParser: true});
