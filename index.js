03bf42d802ccce924416cb556ef1ab3b8c365594
ivanw-yu <ivnyu@mail.sfsu.edu>
2019-02-04 08:10:40 -0800

    Change diretory path to build file in index.js.

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/api/users', users);
 app.use('/api/products', products);
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(port, () => {
